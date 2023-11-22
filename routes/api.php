cd3614ca49a11d65e8e7a069924cda2a407d6c25
cristian <cchura.cpc@gmail.com>
2023-11-21 23:00:10 -0400

    relaciones y autenticación laravel api

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\PersonaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -17,3 +18,14 @@ Route::put("/usuario/{id}", [UserController::class, "funModificar"]);
 Route::delete("/usuario/{id}", [UserController::class, "funEliminar"]);
 
 Route::apiresource("/persona", PersonaController::class);
+
+// Autenticacion Laravel api
+Route::prefix('v1/auth')->group(function(){
+    Route::post("login", [AuthController::class, "funLogin"]);
+    Route::post("register", [AuthController::class, "funRegistro"]);
+
+    Route::middleware("auth:sanctum")->group(function(){
+        Route::get("profile", [AuthController::class, "funPerfil"]);
+        Route::post("logout", [AuthController::class, "funSalir"]);
+    });
+});
